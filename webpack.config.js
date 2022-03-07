f2cb78f43006df9051e59fe761c51e0b147de5af
Valerie-Volfman <valerievolfman@gmail.com>
2022-03-07 16:13:06 +0200

    feat: add postcss loader and configuration file

--- webpack.config.js
+++ webpack.config.js
@@ -35,8 +35,12 @@ module.exports = {
         use: [
           MiniCssExtractPlugin.loader,
           {
-            loader: "css-loader"
+            loader: "css-loader",
+            options: {
+              importLoaders: 1
+            }
           },
+          "postcss-loader"
         ],
       },
       {
